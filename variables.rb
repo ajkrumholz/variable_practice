452d7ae4cedaca47a1f16a6dfb0f68065326cc3d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-05-18 22:44:28 -0400

    Assign all relevant variables

---
+++ variables.rb
@@ -0,0 +1,25 @@
+name = "simon"
+verb = "says"
+quote = "eat your vegetables"
+
+big_number = 12014014
+small_number = 5
+loneliest_number = 1
+
+dollars = 5.21
+cents = 0.12
+pi = 3.14
+
+awake = true
+bedtime = true
+time_for_coffee = false
+
+quote = "eat frozen pizza"
+
+small_number = 3
+
+cents = 0.15
+
+awake = false
+bedtime = false
+time_for_coffee = true
